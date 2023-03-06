75be64a28dcc364acacd1c6a3df94b7a1cd0f0e0
zainuddin25 <zanuk12345>
2023-03-06 18:08:33 +0700

    firts commit

---
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.css']
+})
+export class LandingPageComponent {
+  imageLength: any = [1,2,3,4,5,6,7,8]
+}
